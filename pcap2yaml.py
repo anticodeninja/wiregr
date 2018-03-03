2aade844a5fb42d97d68ed23bc1c82a2577b79b2
anticode.ninja <www.zloi@gmail.com>
2018-03-03 09:14:43 +0700

    Simplify pcap2yaml code

--- pcap2yaml.py
+++ pcap2yaml.py
@@ -42,29 +42,37 @@ class Parser:
             if len(temp) == 0:
                 break
 
+            start_offset = self.__input_file.tell()
+            end_offset = start_offset + self.__unpack(self.fmt_uint32) - 8
+
             info = OrderedDict()
             info['block_type'] = HexInt(struct.unpack(self.fmt_uint32, temp)[0])
             if info['block_type'] == 0x0A0D0D0A:
-                self.__parse_section_header(info)
+                self.__parse_section_header(info, end_offset)
             elif info['block_type'] == 0x00000001:
-                self.__parse_interface_description_block(info)
+                self.__parse_interface_description_block(info, end_offset)
             elif info['block_type'] == 0x00000005:
-                self.__parse_interface_statistic_block(info)
+                self.__parse_interface_statistic_block(info, end_offset)
             elif info['block_type'] == 0x00000006:
-                self.__parse_enhanced_packet_block(info)
+                self.__parse_enhanced_packet_block(info, end_offset)
             else:
-                self.__parse_unknown_block(info)
+                self.__parse_unknown_payload(info, end_offset)
                 print('Unknown block_type', hex(info['block_type']), file=sys.stderr)
+
+            end_offset = self.__input_file.tell()
+            self.__input_file.seek(start_offset, ABSOLUTE)
+            block_total_length_pre = self.__unpack(self.fmt_uint32)
+            self.__input_file.seek(end_offset, ABSOLUTE)
+            block_total_length_post = self.__unpack(self.fmt_uint32)
+            assert block_total_length_pre == block_total_length_post
 
             print(yaml.dump(info, Dumper=CustomDumper), file=self.__output_file)
 
-    def __parse_section_header(self, info):
-        self.__input_file.seek(4, RELATIVE)
+    def __parse_section_header(self, info, end_offset):
+        # re-calculate end_offset after detection endianess
         self.__configure_endianess(self.__unpack(self.fmt_uint32))
         self.__input_file.seek(-8, RELATIVE)
-
-        block_total_length_pre = self.__unpack(self.fmt_uint32)
-        end_payload_offset = self.__input_file.tell() + block_total_length_pre - 12
+        end_offset = self.__input_file.tell() + self.__unpack(self.fmt_uint32) - 12
 
         info['magic'] = HexInt(self.__unpack(self.fmt_uint32))
         info['major_version'] = self.__unpack(self.fmt_uint16)
@@ -76,13 +84,8 @@ class Parser:
             4: ('shb_userappl', self.__unpack_utf8)
         })
 
-        block_total_length_post = self.__unpack(self.fmt_uint32)
-        assert block_total_length_pre == block_total_length_post
 
-
-    def __parse_interface_description_block(self, info):
-        block_total_length_pre = self.__unpack(self.fmt_uint32)
-
+    def __parse_interface_description_block(self, info, end_offset):
         info['link_type'] = self.__unpack(self.fmt_uint16)
         self.__unpack(self.fmt_uint16) # RESERVED
         info['snapshot_length'] = self.__unpack(self.fmt_uint32)
@@ -100,43 +103,34 @@ class Parser:
             interface_param.tsresol = info['options']['if_tsresol']['base'] ** (-info['options']['if_tsresol']['power'])
         self.__interfaces.append(interface_param)
 
-        block_total_length_post = self.__unpack(self.fmt_uint32)
-        assert block_total_length_pre == block_total_length_post
 
-
-    def __parse_enhanced_packet_block(self, info):
-        block_total_length_pre = self.__unpack(self.fmt_uint32)
-        block_payload_end = self.__input_file.tell() + block_total_length_pre - 12
-
+    def __parse_enhanced_packet_block(self, info, end_offset):
         info['interface_id'] = self.__unpack(self.fmt_uint32)
         interface_param = self.__interfaces[info['interface_id']]
         info['datetime'] = self.__unpack_timestamp(interface_param.tsresol)
         info['captured_length'] = self.__unpack(self.fmt_uint32)
         info['packet_length'] = self.__unpack(self.fmt_uint32)
 
+        end_payload_offset = self.__input_file.tell() + info['captured_length']
         if interface_param.link_type == LINKTYPE_ETHERNET:
             self.__parse_aligned(
-                lambda x: self.__parse_ethernet_data(info, x), info['captured_length'], 4)
+                lambda: self.__parse_ethernet_data(info, end_payload_offset),
+                4)
         else:
             info['unknown_payload'] = self.__parse_aligned(
-                lambda x: self.__input_file.read(x), info['captured_length'], 4)
+                lambda: self.__input_file.read(info['captured_length']),
+                4)
             print('Unknown link_type', interface_param.link_type, file=sys.stderr)
 
-        if block_payload_end < self.__input_file.tell():
+        if self.__input_file.tell() < end_offset:
             info['options'] = self.__parse_options({
                 2: ('ebp_flags', lambda x: HexInt(self.__unpack(self.fmt_uint32))),
                 3: ('ebp_hash', lambda x: self.__input_file.read(x)),
                 4: ('epb_dropcount', lambda x: HexInt(self.__unpack(self.fmt_uint64))),
             })
 
-        block_total_length_post = self.__unpack(self.fmt_uint32)
-        assert block_total_length_pre == block_total_length_post
 
-
-    def __parse_interface_statistic_block(self, info):
-        block_total_length_pre = self.__unpack(self.fmt_uint32)
-        block_payload_end = self.__input_file.tell() + block_total_length_pre - 12
-
+    def __parse_interface_statistic_block(self, info, end_offset):
         info['interface_id'] = self.__unpack(self.fmt_uint32)
         info['datetime'] = self.__unpack_timestamp(10 ** -6)
 
@@ -147,37 +141,24 @@ class Parser:
             5: ('isb_ifdrop', lambda x: self.__unpack(self.fmt_uint64)),
         })
 
-        block_total_length_post = self.__unpack(self.fmt_uint32)
-        assert block_total_length_pre == block_total_length_post
 
-
-    def __parse_unknown_block(self, info):
-        block_total_length_pre = self.__unpack(self.fmt_uint32)
-        self.__parse_unknown_payload(info, block_total_length_pre - 12)
-        block_total_length_post = self.__unpack(self.fmt_uint32)
-        assert block_total_length_pre == block_total_length_post
-
-
-    def __parse_ethernet_data(self, info, length):
+    def __parse_ethernet_data(self, info, end_offset):
         ethernet_data = OrderedDict()
         info['ethernet_data'] = ethernet_data
-        start_offset = self.__input_file.tell()
 
         ethernet_data['destination'] = self.__input_file.read(6)
         ethernet_data['source'] = self.__input_file.read(6)
         ethernet_data['type'] = self.__unpack('>H')
 
-        payload_length= length - (self.__input_file.tell() - start_offset)
         if ethernet_data['type'] == TYPE_IPV4:
-            self.__parse_type_ipv4(info, payload_length)
+            self.__parse_type_ipv4(info, end_offset)
         else:
-            self.__parse_unknown_payload(info, payload_length)
+            self.__parse_unknown_payload(info, end_offset)
 
 
-    def __parse_type_ipv4(self, info, length):
+    def __parse_type_ipv4(self, info, end_offset):
         ipv4_data = OrderedDict()
         info['ipv4_data'] = ipv4_data
-        start_offset = self.__input_file.tell()
 
         temp = self.__unpack('B')
         ipv4_data['version'] = temp >> 4
@@ -194,18 +175,17 @@ class Parser:
         ipv4_data['source'] = [x for x in self.__input_file.read(4)]
         ipv4_data['destination'] = [x for x in self.__input_file.read(4)]
 
-        payload_length = length - (self.__input_file.tell() - start_offset)
         if ipv4_data['protocol'] == PROTOCOL_TCP:
-            self.__parse_protocol_tcp(info, payload_length)
+            self.__parse_protocol_tcp(info, end_offset)
         elif ipv4_data['protocol'] == PROTOCOL_UDP:
-            self.__parse_protocol_udp(info, payload_length)
+            self.__parse_protocol_udp(info, end_offset)
         else:
-            self.__parse_unknown_payload(info, payload_length)
+            self.__parse_unknown_payload(info, end_offset)
 
-    def __parse_protocol_tcp(self, info, length):
+
+    def __parse_protocol_tcp(self, info, end_offset):
         tcp_data = OrderedDict()
         info['tcp_data'] = tcp_data
-        start_offset = self.__input_file.tell()
 
         tcp_data['source_port'] = self.__unpack('>H')
         tcp_data['destination_port'] = self.__unpack('>H')
@@ -248,12 +228,10 @@ class Parser:
                     self.__input_file.seek(-2, RELATIVE)
                     tcp_options.append(self.__input_file.read(option_size))
 
-        payload_length = length - (self.__input_file.tell() - start_offset)
-        if payload_length > 0:
-            self.__parse_unknown_payload(info, payload_length)
+        self.__parse_unknown_payload(info, end_offset)
 
 
-    def __parse_protocol_udp(self, info, length):
+    def __parse_protocol_udp(self, info, end_offset):
         udp_data = OrderedDict()
         info['udp_data'] = udp_data
         start_offset = self.__input_file.tell()
@@ -263,13 +241,13 @@ class Parser:
         udp_data['length'] = self.__unpack('>H')
         udp_data['checksum'] = HexInt(self.__unpack('>H'))
 
-        payload_length = udp_data['length'] - (self.__input_file.tell() - start_offset)
-        if payload_length > 0:
-            self.__parse_unknown_payload(info, payload_length)
+        self.__parse_unknown_payload(info, end_offset)
 
 
-    def __parse_unknown_payload(self, info, length):
-        info['unknown_payload'] = self.__input_file.read(length)
+    def __parse_unknown_payload(self, info, end_offset):
+        length = end_offset - self.__input_file.tell()
+        if length > 0:
+            info['unknown_payload'] = self.__input_file.read(length)
 
 
     def __parse_options(self, parsers):
@@ -278,41 +256,39 @@ class Parser:
         while True:
             option_code = self.__unpack(self.fmt_uint16)
             option_length = self.__unpack(self.fmt_uint16)
-            option_format = None
-            option_taken = 0
 
             if option_code == OPT_END:
                 break
             elif option_code == OPT_COMMENT:
                 options['opt_comment'] = self.__parse_aligned(
-                    lambda x: self.__unpack_utf8(x), option_length, 4)
+                    lambda: self.__unpack_utf8(option_length), 4)
             elif option_code in parsers:
                 parser = parsers[option_code]
                 options[parser[0]] = self.__parse_aligned(
-                    lambda x: parser[1](x), option_length, 4)
+                    lambda: parser[1](option_length), 4)
             else:
-                self.__input_file.seek(self.__align(option_length, 4), RELATIVE)
+                self.__input_file.seek(align_value(option_length, 4), RELATIVE)
                 print('Unknown option_code', option_code, file=sys.stderr)
 
         return options
 
-    def __parse_aligned(self, callback, length, align):
-        option_end = self.__input_file.tell() + self.__align(length, align)
-        temp = callback(length)
-        self.__input_file.seek(option_end, ABSOLUTE)
+
+    def __parse_aligned(self, callback, align):
+        start_offset = self.__input_file.tell()
+        temp = callback()
+        end_offset = start_offset + align_value(self.__input_file.tell() - start_offset, align)
+        self.__input_file.seek(end_offset, ABSOLUTE)
         return temp
 
-    def __align(self, size, align):
-        if size % align == 0:
-            return size
-        return size // align * align + align
 
     def __unpack(self, fmt):
         block = self.__input_file.read(struct.calcsize(fmt))
         return struct.unpack(fmt, block)[0]
 
+
     def __unpack_utf8(self, length):
         return self.__input_file.read(length).decode('utf-8')
+
 
     def __unpack_tsresol(self, length):
         temp = self.__unpack(self.fmt_uint8)
@@ -321,9 +297,11 @@ class Parser:
         info['power'] = temp & 0x7F
         return info
 
+
     def __unpack_timestamp(self, tsresol):
         ticks = self.__unpack(self.fmt_uint32) << 32 | self.__unpack(self.fmt_uint32)
         return datetime.datetime(1970, 1, 1) + datetime.timedelta(0, ticks * tsresol)
+
 
     def __configure_endianess(self, magic):
         prefix = '>' if magic == MAGIC else '<'
